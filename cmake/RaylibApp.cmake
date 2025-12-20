4a4e7ff304083ec2b10bc55b5bd484ab548fa65e
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-12-20 16:38:46 +0100

    Rimuove una parte inutile dal .cmake e anche la cartella assets in root

--- cmake/RaylibApp.cmake
+++ cmake/RaylibApp.cmake
@@ -5,11 +5,6 @@ function(add_raylib_app APP_NAME)
 
     target_compile_features(${APP_NAME} PRIVATE cxx_std_20)
 
-    # Asset path automatico
-    target_compile_definitions(${APP_NAME} PRIVATE
-        ASSETS_PATH="${CMAKE_CURRENT_SOURCE_DIR}/assets/"
-    )
-
     # Copia automatica degli assets accanto all'eseguibile
     add_custom_command(
             TARGET ${APP_NAME} POST_BUILD
